9e49c7f7e6d0bf0b3c5f0cd6fefae80769b83b76
arctangerines <arctangerines@icloud.com>
2024-02-11 17:12:28 -0500

    Double blinky

--- src/main.c
+++ src/main.c
@@ -12,10 +12,12 @@ main (void)
   RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOC, ENABLE);
   // Configure Pins
   GPIO_StructInit (&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_Init (GPIOC, &GPIO_InitStructure);
+
+//   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
   // Configure SysTick Timer
   if (SysTick_Config (SystemCoreClock / 1000))
     {
@@ -29,6 +31,7 @@ main (void)
 
       // toggle led
       GPIO_WriteBit (GPIOC, GPIO_Pin_8, (ledval) ? Bit_SET : Bit_RESET);
+      GPIO_WriteBit (GPIOC, GPIO_Pin_9, (ledval) ? Bit_SET : Bit_RESET);
       ledval = 1 - ledval;
       Delay (444); // wait 250ms
     }
