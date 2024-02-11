ccb8e89a5ee595730070f23673f62c096caf0ee2
arctangerines <arctangerines@icloud.com>
2024-02-11 17:40:45 -0500

    Button toggle

--- src/main.c
+++ src/main.c
@@ -2,68 +2,51 @@
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_rcc.h>
 
-void Delay (uint32_t nTime);
+void
+Delay(uint32_t nTime);
 
 int
-main (void)
+main(void)
 {
-  GPIO_InitTypeDef GPIO_InitStructure;
-  // Enable peripheral clocks
-  RCC_APB2PeriphClockCmd (RCC_APB2Periph_GPIOC, ENABLE);
-  // Configure Pins
-  GPIO_StructInit (&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-  GPIO_Init (GPIOC, &GPIO_InitStructure);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    // Enable peripheral clocks
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    // Configure Pins
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-//   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-  // Configure SysTick Timer
-  if (SysTick_Config (SystemCoreClock / 1000))
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    GPIO_InitTypeDef GPIO_Button_InitStruct;
+    GPIO_StructInit(&GPIO_Button_InitStruct);
+    GPIO_Button_InitStruct.GPIO_Pin   = GPIO_Pin_0;
+    GPIO_Button_InitStruct.GPIO_Mode  = GPIO_Mode_IN_FLOATING;
+    GPIO_Button_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOA, &GPIO_Button_InitStruct);
+
+    /* NOTE: This could be edited to make it so its updated every x amount of time 
+             as opposed to as fast it possibly can lol */
+    while (1)
     {
-      while (1)
-        ;
-    }
-
-  while (1)
-    {
-      static int ledval = 0;
-
-      // toggle led
-      GPIO_WriteBit (GPIOC, GPIO_Pin_8, (ledval) ? Bit_SET : Bit_RESET);
-      GPIO_WriteBit (GPIOC, GPIO_Pin_9, (ledval) ? Bit_SET : Bit_RESET);
-      ledval = 1 - ledval;
-      Delay (444); // wait 250ms
-    }
-}
-
-// Timer code
-static __IO uint32_t TimingDelay;
-
-void
-Delay (uint32_t nTime)
-{
-  TimingDelay = nTime;
-  while (TimingDelay != 0)
-    ;
-}
-
-void
-SysTick_Handler (void)
-{
-  if (TimingDelay != 0x00)
-    {
-      TimingDelay--;
+        static int ledval = 0;
+        if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0))
+        {
+            // GPIO_WriteBit(GPIOC, GPIO_Pin_8, Bit_SET);
+            GPIO_WriteBit(GPIOC, GPIO_Pin_8, (ledval) ? Bit_SET:Bit_RESET);
+        }
+        ledval = 1 - ledval;
     }
 }
 
 #ifdef USE_FULL_ASSERT
 void
-assert_failed (uint8_t *file, uint32_t line)
+assert_failed(uint8_t* file, uint32_t line)
 {
-  /* Infinite loop*/
-  /* GDB can help us find out why this happened*/
-  while (1)
-    ;
+    /* Infinite loop*/
+    /* GDB can help us find out why this happened*/
+    while (1)
+        ;
 }
 #endif
